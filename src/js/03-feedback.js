441e2ac8723a23bbd4998afa24f9f0faeba283ea
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2023-08-17 19:40:22 +0200

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+// Select the form element
+const form = document.querySelector('.feedback-form');
 
+// Select the email and message fields
+const emailField = form.querySelector('input[name="email"]');
+const messageField = form.querySelector('textarea[name="message"]');
+
+// Load form state from storage on page load
+window.addEventListener('load', () => {
+  const savedState = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (savedState) {
+    emailField.value = savedState.email || '';
+    messageField.value = savedState.message || '';
+  }
+});
+
+// Throttle the update function to prevent frequent storage updates
+const saveFormState = _.throttle(() => {
+  const currentState = {
+    email: emailField.value,
+    message: messageField.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
+}, 500);
+
+// Save form state on input event
+form.addEventListener('input', saveFormState);
+
+// Clear storage and form fields on form submit
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('Form submitted!', {
+    email: emailField.value,
+    message: messageField.value,
+  });
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+});
