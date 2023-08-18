6aeba1fb46f561716a41dd615f7afd389ff0049b
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2023-08-18 19:34:42 +0200

    task 2 - add event 'ended'

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,20 +6,41 @@ const player = new Player('vimeo-player');
 player.on('timeupdate', function(data) {
     const currentTime = data.seconds;
     localStorage.setItem('videoplayer-current-time', currentTime);
-  });
+});
 
-  const savedTime = localStorage.getItem('videoplayer-current-time');
-  player.setCurrentTime(savedTime);
+const savedTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(savedTime);
 
-  function updateTime(currentTime) {
+function updateTime(currentTime) {
     localStorage.setItem('videoplayer-current-time', currentTime);
-  }
-  
-  const throttledUpdateTime = throttle(updateTime, 1000); // 1000 ms = 1s
-  
-  player.on('timeupdate', function(data) {
+}
+
+const throttledUpdateTime = throttle(updateTime, 1000); // 1000 ms = 1s
+
+player.on('timeupdate', function(data) {
     const currentTime = data.seconds;
     throttledUpdateTime(currentTime);
-  });
+});
+
+function checkSavedTime() {
+    const savedTime = localStorage.getItem('videoplayer-current-time');
+    player.getDuration().then(function(duration) {
+        if (savedTime === null || savedTime > duration) {
+            player.setCurrentTime(0);
+        }
+    });
+}
+
+player.on('loaded', function() {
+    checkSavedTime();
+});
+
+player.on('play', function() {
+    checkSavedTime();
+});
+
+player.on('ended', function() {
+    player.setCurrentTime(0);
+});
 
 
